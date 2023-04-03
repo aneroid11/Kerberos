6d78331de713ce7c73ce9de82f9f9f7f9b248a08
aneroid11 <a-e-v2003@yandex.by>
2023-04-03 23:36:45 +0300

    initial key permutation

--- des.py
+++ des.py
@@ -2,12 +2,31 @@
 
 
 class Des:
+    def _correct_pc_1(self):
+        for i in range(len(self._pc_1)):
+            self._pc_1[i] -= 1
+
     def __init__(self, plain_message: str, key: bytearray):
+        self._pc_1 = [
+            57, 49, 41, 33, 25, 17, 9,
+            1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27,
+            19, 11, 3, 60, 52, 44, 36,
+            63, 55, 47, 39, 31, 23, 15,
+            7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29,
+            21, 13, 5, 28, 20, 12, 4
+        ]
+        self._correct_pc_1()
+
         self._plain_data = bytearray(plain_message, "utf-8")
         self._key = key
         self._key_bits = []
+        self._key_bits_56 = []
 
         self._key_to_key_bits()
+        self._key_initial_permutation()
+        print(self._key_bits_56)
         # print(key.hex())
         self._encrypted_data = bytearray()
 
@@ -24,6 +43,10 @@ class Des:
             self._key_bits.append(bit)
             print(bit, end="")
         print()
+
+    def _key_initial_permutation(self):
+        for i in range(56):
+            self._key_bits_56.append(self._key_bits[self._pc_1[i]])
 
     def _append_zeros_to_plain_data(self):
         plain_data_len = len(self._plain_data)
