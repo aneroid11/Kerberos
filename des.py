712035a4896686605f9672c9628bdec2193b5285
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 00:36:54 +0300

    initial permutation

--- des.py
+++ des.py
@@ -87,9 +87,28 @@ class Des:
         for _ in range(zeros_to_add):
             plain_data.append(0)
 
+    def _msg_initial_permutation(self, curr_block_bits: list) -> list:
+        ip = [
+            58, 50, 42, 34, 26, 18, 10, 2,
+            60, 52, 44, 36, 28, 20, 12, 4,
+            62, 54, 46, 38, 30, 22, 14, 6,
+            64, 56, 48, 40, 32, 24, 16, 8,
+            57, 49, 41, 33, 25, 17, 9, 1,
+            59, 51, 43, 35, 27, 19, 11, 3,
+            61, 53, 45, 37, 29, 21, 13, 5,
+            63, 55, 47, 39, 31, 23, 15, 7
+        ]
+        ip = [x - 1 for x in ip]
+
+        perm_bits = [curr_block_bits[ip[i]] for i in range(64)]
+        return perm_bits
+
+
     def _encrypt_block(self, curr_block: bytearray) -> bytearray:
         curr_block_bits = [self._access_bit(curr_block, i) for i in range(64)]
-        print(curr_block_bits)
+        initial_permutation = self._msg_initial_permutation(curr_block_bits)
+        print(initial_permutation)
+
         return curr_block
 
     def encrypt(self, plain_data: bytearray) -> bytearray:
