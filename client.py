41ada9716c2f456e4b7039f3cdbc9f3738b2bb74
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 17:30:07 +0300

    dummy udp server and client

--- client.py
+++ client.py
@@ -5,15 +5,11 @@ class Client:
     MAX_DATA_LEN = 1024
 
     def __init__(self):
-        self._sock = socket.socket()
-        self._sock.connect(('localhost', 50000))
-        print("client connected!")
+        self._sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        print("client is up!")
 
     def __del__(self):
         self._sock.close()
 
     def send_data(self):
-        self._sock.send("hello world")
-
-        response = self._sock.recv(Client.MAX_DATA_LEN)
-        print(response)
+        self._sock.sendto(str.encode("hello world", "utf-8"), ("localhost", 50000))
--- server_service.py
+++ server_service.py
@@ -5,22 +5,15 @@ class ServerService:
     MAX_DATA_LEN = 1024
 
     def __init__(self):
-        self._sock = socket.socket()
+        self._sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self._sock.bind(('', 50000))
 
-        print("listen...")
-        self._sock.listen(1)
-        print("waiting for accept...")
-        self._conn_sock, self._client_addr = self._sock.accept()
-        print("connected:", self._client_addr)
+        print("UDP server up and listening")
 
     def __del__(self):
         self._sock.close()
-        self._conn_sock.close()
 
     def receive_data(self):
-        while True:
-            data = self._conn_sock.recv(ServerService.MAX_DATA_LEN)
-            if not data:
-                break
-            self._conn_sock.send(data.upper())
+        # blocking call
+        message, addr = self._sock.recvfrom(ServerService.MAX_DATA_LEN)
+        print(f"message {message} from {addr}")
