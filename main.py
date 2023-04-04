c8e49f436a52f9ed1c34c9d8df89075317e2d6c9
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 16:18:55 +0300

    before implementing the main part

--- main.py
+++ main.py
@@ -11,13 +11,6 @@ def main():
     msg_decrypted = encryptor.encrypt(msg_encrypted, True)
     print(msg_decrypted)
 
-    # msg = 0x0123456789ABCDEF
-    # msg = 0x85e813540f0ab405  # encrypted version
-    #
-    # msg_encrypted = encryptor.encrypt(bytearray(msg.to_bytes(8, "big")), True)
-    # msg_encr_num = int.from_bytes(msg_encrypted, "big")
-    # print(hex(msg_encr_num))
-
 
 if __name__ == '__main__':
     main()
