270bdab5b2f1532c471c89c5a4ac778f54d6bbde
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 12:02:06 +0300

    bitlist to num and num to bitlist

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     # print(encryptor.encrypt("55"))
     print(encryptor.encrypt(bytearray(msg.to_bytes(8, "big"))))
     # print(encryptor.decrypt(encryptor.encrypt("55")))
+    print(encryptor.num_to_bitlist(9))
 
 
 if __name__ == '__main__':
