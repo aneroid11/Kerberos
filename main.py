d693bc11b5763c7bf3d1255258fd2a01f6ecab5c
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 16:27:31 +0300

    dummy C, KDC, SS

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from des import Des
 
 
 def main():
-    key = bytearray(0x133457799BBCDFF1.to_bytes(8, "big"))  # BIG
-    encryptor = Des(key)
-
-    msg = bytearray("passwordimportant", "utf-8")
-    msg_encrypted = encryptor.encrypt(msg)
-    print(msg_encrypted)
-    msg_decrypted = encryptor.encrypt(msg_encrypted, True)
-    print(msg_decrypted)
+    # key = bytearray(0x133457799BBCDFF1.to_bytes(8, "big"))  # BIG
+    # encryptor = Des(key)
+    #
+    # msg = bytearray("passwordimportant", "utf-8")
+    # msg_encrypted = encryptor.encrypt(msg)
+    # print(msg_encrypted)
+    # msg_decrypted = encryptor.encrypt(msg_encrypted, True)
+    # print(msg_decrypted)
+    print("hello world")
 
 
 if __name__ == '__main__':
