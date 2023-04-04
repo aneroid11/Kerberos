5cdaa1641a04644de52889b56c32f7d27b064e77
aneroid11 <a-e-v2003@yandex.by>
2023-04-04 12:17:26 +0300

    finished f()

--- des.py
+++ des.py
@@ -207,9 +207,20 @@ class Des:
             end = start + 8
             s_boxed += self._calculate_s_box(i, xored[start:end])
 
-        print(s_boxed)
+        p = [
+            16,   7,  20,  21,
+            29,  12,  28,  17,
+            1,  15,  23,  26,
+            5,  18,  31,  10,
+            2,   8,  24,  14,
+            32,  27,   3,   9,
+            19,  13,  30,   6,
+            22,  11,   4,  25
+        ]
+        p = [x - 1 for x in p]
 
-        return xored
+        output = [s_boxed[p[i]] for i in range(32)]
+        return output
 
     def _xor_bitlist(self, l0: list, l1: list) -> list:
         size = len(l0)
